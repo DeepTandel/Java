59cd255b0c2335d55ae1171c87b6c6de49a44e55
DeepTandel <deeptandel1405@gmail.com>
2024-01-03 22:18:55 +0530

    code

--- Basic/Dem.java
+++ Basic/Dem.java
@@ -1,3 +1,10 @@
 public class Dem {
-    
+    public static void main(String[] args) {
+        int n=5;
+        while(n>0){
+          System.out.print("Hello \t"); 
+          n--; 
+        }
+        
+    }
 }
